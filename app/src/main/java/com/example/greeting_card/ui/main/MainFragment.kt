04c8f91bd2559c2981e716f9b321e4c93498135d
VladVR1 <vladvarcenko11@gmail.com>
2021-04-21 09:54:00 +0300

    create switchImage

--- app/src/main/java/com/example/greeting_card/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/greeting_card/ui/main/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.greeting_card.R
 import com.example.greeting_card.databinding.FragmentMainBinding
@@ -16,19 +17,30 @@ class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
     private lateinit var viewModel: InformationViewModel
+    private var position: Int = 0
+    private val arrayImageSwitcher = arrayOf(
+        R.drawable.fjb1,
+        R.drawable.fjb2,
+        R.drawable.fjb3,
+        R.drawable.fjb4
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        viewModel = ViewModelProvider(this).get(InformationViewModel::class.java)
         binding = FragmentMainBinding.inflate(inflater)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.mainScreenButtonLaunch.setOnClickListener { verificationEditText() }
+        switchImage()
     }
 
     private fun verificationEditText() {
@@ -49,6 +61,29 @@ class MainFragment : Fragment() {
     private fun switchImage() {
         val imageSwitcher = binding.mainScreenImageSwitcher
 
+        when {
+
+            R.id.buttonNext.equals(setPositionNext()) -> imageSwitcher.setImageResource(
+                arrayImageSwitcher[position]
+            )
+            R.id.buttonBack.equals(setPositionBack()) -> imageSwitcher.setImageResource(
+                arrayImageSwitcher[position]
+            )
+        }
+    }
+
+    private fun setPositionNext() {
+        position++
+        if (position > arrayImageSwitcher.size - 1) {
+            position = 0
+        }
+    }
+
+    private fun setPositionBack() {
+        position--
+        if (position < 0) {
+            position = arrayImageSwitcher.size - 1
+        }
     }
 }
 
